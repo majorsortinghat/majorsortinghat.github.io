e061f312ad621bab6f563cda1d5e8dc1176847e8
casparmkerr <casparmkerr@gmail.com>
2018-12-02 01:02:10 -0500

    Added lines to viz

---
+++ SortingHat_ResultsPage.js
@@ -0,0 +1,64 @@
+console.log("hey");
+
+var w = window.innerWidth;
+var h = window.innerHeight;
+var majors = cleanData(data);
+var majors_len = Object.keys(majors).length;
+console.log(majors_len);
+var minScreensDimension = Math.min.apply(null, [w, h]);
+console.log(minScreensDimension);
+var center = [w / 2, h / 2];
+
+//console.log(data);
+
+function setup() {
+    var myCanvas = createCanvas(w, h);
+    myCanvas.parent('visualization');
+
+}
+
+function draw() {
+
+    ellipseMode(CENTER); // Set ellipseMode to CENTER
+    fill(255, 0, 50); // Set fill
+    noStroke();
+    ellipse(center[0], center[1], 50); // Draw gray ellipse using CENTER mode
+
+
+    stroke(150);
+    //var majors = cleanData(data);
+    var n = 1;
+    for (var key in majors) {
+
+        line(center[0], center[1], center[0] + (minScreensDimension / 7) * Math.cos(2 * Math.PI * n / majors_len) * majors[key], center[1] + (minScreensDimension / 7) * Math.sin(2 * Math.PI * n / majors_len) * majors[key]);
+        n++;
+        //console.log(key, majors[key]);
+    }
+
+}
+
+
+
+function cleanData(dirtyData) {
+    //console.log(dirtyData);
+    var cleanDict = {};
+
+
+    var values = Object.keys(dirtyData).map(function(key) {
+        return dirtyData[key];
+    });
+    var min_value = Math.min.apply(null, values);
+    //console.log(min_value);
+    for (var key in data) {
+        //console.log(key);
+        // check if the property/key is defined in the object itself, not in parent
+        if (dirtyData.hasOwnProperty(key)) {
+            if (dirtyData[key] <= min_value * 5) {
+                //console.log(dirtyData[key]);
+                cleanDict[key] = dirtyData[key] / min_value;
+            }
+            //console.log(key, dictionary[key]);
+        }
+    }
+    return cleanDict;
+}
--- SortingHat_ResultsPage.php
+++ SortingHat_ResultsPage.php
@@ -17,11 +17,8 @@
 <!-- Latest compiled JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     
-<script src="vis/dist/vis.js"></script>
-  <link href="vis/dist/vis.css" rel="stylesheet" type="text/css" />
 
-  <script src="https://d3js.org/d3-path.v1.min.js"></script>
-<script src="https://d3js.org/d3-shape.v1.min.js"></script>
+
 
     
 </head>
@@ -32,49 +29,23 @@
 
     <?php include "scoreCalc.php";?>
 
+
     <div id="visualization">
 
 
 
     </div>
 
-    <script>
 
-        var line = d3.line();
-
-    </script>
     <script type="text/javascript">
 
         var data = <?php
-        echo json_encode(topThree($data, $replies), JSON_HEX_TAG); ?>; //Don't forget the extra semicolon!
+        echo json_encode(compareValues($data, $replies), JSON_HEX_TAG); ?>; //Don't forget the extra semicolon!
 
-        console.log(data);
+        //console.log(data);
 
 
 
-    var container = document.getElementById('visualization');
-    var items = [
-        {x: '2014-06-11', y: 10},
-        {x: '2014-06-12', y: 25},
-        {x: '2014-06-13', y: 30},
-        {x: '2014-06-14', y: 10},
-        {x: '2014-06-15', y: 15},
-        {x: '2014-06-16', y: 30}
-    ];
-
-    var dataset = new vis.DataSet(items);
-    var options = {
-        style:'bar',
-        barChart: {width:50, align:'center'}, // align: left, center, right
-        drawPoints: false,
-        dataAxis: {
-            icons:true
-        },
-        orientation:'top',
-        start: '2014-06-10',
-        end: '2014-06-18'
-    };
-    //var graph2d = new vis.Graph2d(container, items, options);
 
 
 
@@ -83,6 +54,10 @@
         console.log("hey");
         
     </script>
+
+    <script src="https://d3js.org/d3.v4.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.2/p5.js"></script>
+    <script src="SortingHat_ResultsPage.js"></script>
 </body>
 
 
